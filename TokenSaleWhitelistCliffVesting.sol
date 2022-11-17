19de2ed978976c33481bec32b702d322664ee10c
opendeliverydao <devteam@opendelivery.app>
2022-11-17 22:21:00 +0000

    fix tokensale

--- TokenSaleWhitelistCliffVesting.sol
+++ TokenSaleWhitelistCliffVesting.sol
@@ -29,7 +29,7 @@ contract TokenSaleWhitelistCliffVesting is Pausable, AccessControl,TokenAllocati
 
     IERC20 public tokenAddress;
     address public sellerAddress;
-    address public taxAddress;
+    uint256 public currentPrice;
     uint256 public currentErc20Price;
     bool public useWhitelist;
     uint256 public minTokenAmount;
@@ -44,11 +44,13 @@ contract TokenSaleWhitelistCliffVesting is Pausable, AccessControl,TokenAllocati
     constructor(
       address _tokenAddress,
       address _sellerAddress, 
-      address _taxAddress,
+      uint256 _currentPrice, 
       IERC20 _payment_token,
       uint256 _currentErc20Price,
+      address[] memory _whitelist,
       uint256 _startTs,
       uint256 _endTs,
+      bool _useWhitelist,
       uint256[] memory _tokenAllocation
       ) 
     TokenAllocation(
@@ -61,6 +63,7 @@ contract TokenSaleWhitelistCliffVesting is Pausable, AccessControl,TokenAllocati
      { 
         require(_sellerAddress != address(0) && _sellerAddress != address(this));
         require(_tokenAddress != address(0) && _tokenAddress != address(this));
+        require(_currentPrice > 0);
 
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(PAUSER_ROLE, msg.sender);
@@ -68,14 +71,17 @@ contract TokenSaleWhitelistCliffVesting is Pausable, AccessControl,TokenAllocati
         maxTokenAmount=_tokenAllocation[1];
         tokenAddress = IERC20(_tokenAddress);
         sellerAddress = _sellerAddress;
+        currentPrice = _currentPrice;
         currentErc20Price = _currentErc20Price;
         startTimestamp=_startTs;
         _setIcoTimestamp(_endTs);
         emit StartSales(_startTs);
         endTimestamp=_endTs;
         paymentToken=_payment_token;
-        useWhitelist=false;
-        taxAddress = _taxAddress;
+        useWhitelist=_useWhitelist;
+        if (useWhitelist){
+          addToWhitelist(_whitelist);
+        }
         
     }
 
@@ -88,6 +94,29 @@ contract TokenSaleWhitelistCliffVesting is Pausable, AccessControl,TokenAllocati
     _unpause();
     }
 
+    function purchaseToken(uint256 _quantity) public payable whenNotPaused afterStartSales beforeEndSales {
+        require(_quantity >= minTokenAmount, "Sale: amount less than min");
+        require(_quantity <= maxTokenAmount, "Sale: amount greater than max");
+        uint256 sellerBalance=(IERC20(tokenAddress)).balanceOf(sellerAddress);
+        require(
+          _getUserTotalTokens(msg.sender) + _quantity <= maxTokenAmount,
+          "Sale: total greater than max"
+        );
+        require(
+          tokenSold + _quantity <= sellerBalance,
+          "Sale: total TOKEN on sale reached"
+        );
+
+        require((!useWhitelist || isWhitelisted[msg.sender]), "Wallet is not whitelisted");
+        require(msg.sender != address(0) && msg.sender != address(this));
+        require(_quantity >= 1);
+        require(msg.value >= currentPrice * (_quantity/1e18));
+        require(currentPrice>0);
+
+        emit Received(msg.sender, msg.value);
+        tokenSold += _quantity;
+        _updateUserTokenAllocation(msg.sender, _quantity);
+    }
 
     function purchaseTokenWithErc20(uint256 _quantity) public payable whenNotPaused afterStartSales beforeEndSales {
         require(_quantity >= minTokenAmount, "Sale: amount less than min");
@@ -106,13 +135,7 @@ contract TokenSaleWhitelistCliffVesting is Pausable, AccessControl,TokenAllocati
         require(_quantity >= 1);
         require(currentErc20Price>0);
 
-        uint256 totalPayment=currentErc20Price*(_quantity/1e18);
-        uint256 taxPay= (totalPayment/100)*2;
-        uint256 sellerPayment=totalPayment-taxPay;
-
-        paymentToken.transferFrom(msg.sender, sellerAddress, sellerPayment);
-        paymentToken.transferFrom(msg.sender, taxAddress, taxPay);
-
+        paymentToken.transferFrom(msg.sender, sellerAddress, currentErc20Price*(_quantity/1e18));
         emit ReceivedErc20(msg.sender, currentErc20Price*(_quantity/1e18));
         tokenSold += _quantity;
         _updateUserTokenAllocation(msg.sender, _quantity);
@@ -124,7 +147,11 @@ contract TokenSaleWhitelistCliffVesting is Pausable, AccessControl,TokenAllocati
         payable(_payee).transfer(_amount);
         emit Sent(_payee, _amount, address(this).balance);
     }    
- 
+
+    function setCurrentPrice(uint256 _currentPrice) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(_currentPrice > 0);
+        currentPrice = _currentPrice;
+    }    
 
     function setCurrentErc20Price(uint256 _currentErc20Price) public onlyRole(DEFAULT_ADMIN_ROLE) {
         require(_currentErc20Price > 0);
